c9e1a6fc9d8eb7a8ae32006b084d3f349e25be4f
Sean D Ward <119538683+Sean-Ward-The-Albino@users.noreply.github.com>
2023-09-27 11:46:28 -0700

    minor changes

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-// Function to fade in elements
 function fadeInElements() {
-    // Get elements to fade in
     const elementsToFadeIn = document.querySelectorAll('.fade-in');
 
-    // Loop through the elements and apply the fade-in class
     elementsToFadeIn.forEach((element, index) => {
-        // Delay the animation for each element to create a staggered effect
         setTimeout(() => {
             element.classList.add('fade-in-animation');
-        }, index * 300); // Adjust the delay duration as needed
+        }, index * 300); 
     });
 }
 
-// Add event listener to trigger the fade-in when the page is fully loaded
 window.addEventListener('load', () => {
     fadeInElements();
 });
